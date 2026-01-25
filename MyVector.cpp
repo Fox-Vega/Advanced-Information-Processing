a00d1fa4fabeaad7ac5fede0dae0bf9920965c5e
Fox-VEGA <Fox.Vega.Soft@gmail.com>
2026-01-25 16:22:02 +0900

    Added MyVector.cpp

---
+++ MyVector.cpp
@@ -0,0 +1,69 @@
+//Created by Riku Konishi
+
+#pragma once
+#include <Arduino.h>
+
+
+class MyVECTOR {
+    public:
+        float get_azimuth(float x, float y); //Get azimuth(0-360) from vector coordinates
+        float get_magnitude(float x, float y); //Get magnitude from vector coordinates
+        void get_cord(float azimuth, float magnitude); //Convert azimuth(0-360) and magnitude into vector coordinates
+        float get_x(); //Get vector_x
+        float get_y(); //Get vector_y
+        float get_vectordegree(float ax, float ay, float bx, float by); //Calculate the angle difference between two vectors
+
+    private:
+        int x, y;
+        double theta;
+        double azimuth;
+        #define M_PI 3.14159265358979323846 //To be safe
+};
+
+
+float MyVECTOR::get_azimuth(float x, float y) {
+    if (x == 0 && y == 0) {
+        azimuth = 0;
+    } else {
+        theta = atan2(y, x);
+        azimuth = 90.0f - (theta * 180.0f / M_PI);
+        if (azimuth < 0) azimuth += 360;
+        else if (azimuth >= 360) azimuth -= 360;
+    }
+    return azimuth;
+}
+
+float MyVECTOR::get_magnitude(float x, float y) {
+    return sqrtf(x * x + y * y);
+}
+
+void MyVECTOR::get_cord(float azimuth, float magnitude) {
+    if (magnitude < 0) { //Reverse
+        azimuth += 180;
+        if (azimuth >= 360) azimuth -= 360;
+        magnitude = -magnitude;
+    }
+    theta = 90 - azimuth;
+    while (theta > 180) theta -= 360;
+    while (theta < -180) theta += 360;
+    x = round(cos(theta * M_PI / 180.0) * magnitude);
+    y = round(sin(theta * M_PI / 180.0) * magnitude);
+}
+
+float MyVECTOR::get_x() {
+    return x;
+}
+
+float MyVECTOR::get_y() {
+    return y;
+}
+
+float MyVECTOR::get_vectordegree(float ax, float ay, float bx, float by) {
+    float a_magnitude = myvector.get_magnitude(ax, ay);
+    float b_magnitude = myvector.get_magnitude(bx, by);
+    if (a_magnitude == 0.0f || b_magnitude == 0.0f) return 0;
+    double cos_theta = (ax * bx + ay * by) / (a_magnitude * b_magnitude);
+    if (cos_theta > 1.0f) cos_theta = 1.0f;
+    if (cos_theta < -1.0f) cos_theta = -1.0f;
+    return degrees(acos(cos_theta));
+}
